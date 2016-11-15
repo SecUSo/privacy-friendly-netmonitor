67b80f2041bcf1773977b20ee2539e28417960cf
Felix <fs@mbits.solutions>
2016-11-16 00:38:39 +0100

    repacked project

---
+++ app/src/main/java/org/secuso/privacyfriendlytlsmetric/ConnectionAnalysis/Filter/Tls.java
@@ -0,0 +1,61 @@
+/*
+    TLSMetric
+    - Copyright (2015, 2016) Felix Tsala Schiller
+
+    ###################################################################
+
+    This file is part of TLSMetric.
+
+    TLSMetric is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TLSMetric is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TLSMetric.  If not, see <http://www.gnu.org/licenses/>.
+
+    Diese Datei ist Teil von TLSMetric.
+
+    TLSMetric ist Freie Software: Sie können es unter den Bedingungen
+    der GNU General Public License, wie von der Free Software Foundation,
+    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+    TLSMetric wird in der Hoffnung, dass es nützlich sein wird, aber
+    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+    Siehe die GNU General Public License für weitere Details.
+
+    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
+package org.secuso.privacyfriendlytlsmetric.ConnectionAnalysis.Filter;
+
+/**
+ * Protocol filter for all TLS versions. Can hold a message type identifier.
+ */
+public class Tls extends Filter {
+
+    public TlsProtocol mSubProtocol;
+    public int mVersion;
+
+    public Tls(Protocol protocol, int severity, String description, TlsProtocol subProtocol, int version) {
+        super(protocol, severity, description);
+        checkCypher = true;
+        mSubProtocol = subProtocol;
+        mVersion = version;
+    }
+
+    public enum TlsProtocol {
+        HANDSHAKE,
+        CHANGE_CYPHER,
+        ALERT,
+        APP_DATA
+    }
+}
