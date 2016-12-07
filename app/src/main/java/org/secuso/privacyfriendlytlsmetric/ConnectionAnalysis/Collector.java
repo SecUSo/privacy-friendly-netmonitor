7a1e7ec1f3a78f665261be3879f3267c9271acf0
Felix Tsala Schiller <felixschiller@posteo.de>
2016-12-07 12:28:44 +0100

    Added Package Resolution

--- app/src/main/java/org/secuso/privacyfriendlytlsmetric/ConnectionAnalysis/Collector.java
+++ app/src/main/java/org/secuso/privacyfriendlytlsmetric/ConnectionAnalysis/Collector.java
@@ -1,25 +1,35 @@
 package org.secuso.privacyfriendlytlsmetric.ConnectionAnalysis;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import org.secuso.privacyfriendlytlsmetric.Assistant.Const;
+import org.secuso.privacyfriendlytlsmetric.Assistant.ContextStorage;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
 /**
  * Collector class collects data from the services and processes it for inter process communication
  * with the UI.
  */
-
 public class Collector {
 
-    private static ArrayList<Report> mReportList;
     public static Report[] mReportArray;
-    //Pushed the newest availiable information to binder. The param indicates the update-strategy
-    //of the reports. See Detector for further information
+    public static HashMap<Integer, PackageInformation> mUidPackageMap = new HashMap<>();
 
+    private static ArrayList<Report> mReportList;
+
+    //Pushed the newest availiable information as deep copy.
     public static void provideReports(){
         updateReportList();
 
@@ -42,14 +52,28 @@ public class Collector {
             reportList.add(Detector.sReportMap.get(i));
         }
         mReportList = deepCloneReportList(reportList);
-        //mReportList = (ArrayList<Report>) mReportList.clone();
     }
 
     //Process all Records in the List, based on a passive service
     private static void processPassive() {
-        //TODO: Process Packet information
+        fillPackageInformation();
+        resolveHosts();
     }
 
+    private static void resolveHosts() {
+        //TODO: implement, use permission swich
+    }
+
+    private static void fillPackageInformation(){
+        //Get Package Information
+        for (int i = 0; i < mReportList.size(); i++){
+            Report report = mReportList.get(i);
+            updatePackage(report.getUid());
+            PackageInformation pi = mUidPackageMap.get(report.getUid());
+            report.setPid(pi.pid);
+            report.setPackageName(pi.packageName);
+        }
+    }
     //Make a deep copy of the report list
     private static ArrayList<Report> deepCloneReportList(ArrayList<Report> reportList) {
         ArrayList<Report> cloneList = new ArrayList<>();
@@ -66,5 +90,33 @@ public class Collector {
             e.printStackTrace();
         }
         return cloneList;
+    }
+
+    //Updates the PackageInformation hash map with new entries.
+    private static void updatePackage(int uid) {
+        if (!mUidPackageMap.containsKey(uid)){
+            PackageManager pm = ContextStorage.getContext().getPackageManager();
+            ActivityManager am = (ActivityManager) ContextStorage.getContext().getSystemService(Context.ACTIVITY_SERVICE);
+
+            PackageInformation pi = new PackageInformation();
+            pi.uid = uid;
+
+            List<ActivityManager.RunningAppProcessInfo> activeApps = am.getRunningAppProcesses();
+            for (int i = 0; i < activeApps.size(); i++) {
+                ActivityManager.RunningAppProcessInfo info = activeApps.get(i);
+                if(info.uid == uid ){
+                    try {
+                        String[] list = info.pkgList;
+                        pi.packageName = list[0];
+                        pi.pid = info.pid;
+                        pi.icon = pm.getApplicationIcon(pi.packageName);
+                        mUidPackageMap.put(uid, pi);
+                    } catch (PackageManager.NameNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }
     }
 }
