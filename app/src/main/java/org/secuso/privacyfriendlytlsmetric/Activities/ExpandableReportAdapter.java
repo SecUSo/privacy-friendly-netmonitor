d7f9735538c47241e950847979d73e52fba0953d
Felix Tsala Schiller <felixschiller@posteo.de>
2016-12-13 21:22:03 +0100

    implemented swipe refresh layout functionallity

--- app/src/main/java/org/secuso/privacyfriendlytlsmetric/Activities/ExpandableReportAdapter.java
+++ app/src/main/java/org/secuso/privacyfriendlytlsmetric/Activities/ExpandableReportAdapter.java
@@ -55,7 +55,7 @@ class ExpandableReportAdapter extends BaseExpandableListAdapter {
             text1 = "" + r.getRemoteAdd().getHostAddress();
         }
         //final String text2 = "<No Metric info yet>";
-        final String text2 = r.getType() + " " +r.getRemoteAdd().getHostAddress()
+        final String text2 = r.getType() + " " + r.getRemoteAdd().getHostAddress()
                 + ":" + r.getRemotePort();
 
         if (convertView == null) {
