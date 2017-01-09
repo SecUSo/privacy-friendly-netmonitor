7fb66407b72392b0896ec0134e7cc9999d7af1a6
Felix <fs@mbits.solutions>
2017-01-09 01:14:22 +0100

    Help Dump Code cleanup

--- app/src/main/java/org/secuso/privacyfriendlytlsmetric/Activities/HelpDataDump.java
+++ app/src/main/java/org/secuso/privacyfriendlytlsmetric/Activities/HelpDataDump.java
@@ -13,57 +13,57 @@ import java.util.List;
  * last access 27th October 2016
  */
 
-public class HelpDataDump {
+class HelpDataDump {
 
     private Context context;
 
-    public HelpDataDump(Context context) {
+    HelpDataDump(Context context) {
         this.context = context;
     }
 
-    public HashMap<String, List<String>> getDataGeneral() {
-        HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
+    HashMap<String, List<String>> getDataGeneral() {
+        HashMap<String, List<String>> expandableListDetail = new HashMap<>();
 
-        List<String> general = new ArrayList<String>();
+        List<String> general = new ArrayList<>();
         general.add(context.getResources().getString(R.string.help_whatis_answer));
         expandableListDetail.put(context.getResources().getString(R.string.help_whatis), general);
         return expandableListDetail;
     }
 
-    public HashMap<String, List<String>> getDataFeatures() {
+    HashMap<String, List<String>> getDataFeatures() {
         HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
 
-        List<String> features1 = new ArrayList<String>();
+        List<String> features1 = new ArrayList<>();
         features1.add(context.getResources().getString(R.string.help_feature_one_answer));
         expandableListDetail.put(context.getResources().getString(R.string.help_feature_one), features1);
 
-        List<String> features2 = new ArrayList<String>();
+        List<String> features2 = new ArrayList<>();
         features2.add(context.getResources().getString(R.string.help_feature_two_answer));
         expandableListDetail.put(context.getResources().getString(R.string.help_feature_two), features2);
 
-        List<String> features3 = new ArrayList<String>();
+        List<String> features3 = new ArrayList<>();
         features3.add(context.getResources().getString(R.string.help_feature_three_answer));
         expandableListDetail.put(context.getResources().getString(R.string.help_feature_three), features3);
 
-        List<String> features4 = new ArrayList<String>();
+        List<String> features4 = new ArrayList<>();
         features4.add(context.getResources().getString(R.string.help_feature_four_answer));
         expandableListDetail.put(context.getResources().getString(R.string.help_feature_four), features4);
 
-        List<String> features5 = new ArrayList<String>();
+        List<String> features5 = new ArrayList<>();
         features5.add(context.getResources().getString(R.string.help_feature_five_answer));
         expandableListDetail.put(context.getResources().getString(R.string.help_feature_five), features5);
 
         return expandableListDetail;
     }
 
-    public HashMap<String, List<String>> getDataPrivacy() {
-        HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
+    HashMap<String, List<String>> getDataPrivacy() {
+        HashMap<String, List<String>> expandableListDetail = new HashMap<>();
 
 
-        List<String> privacy = new ArrayList<String>();
+        List<String> privacy = new ArrayList<>();
         privacy.add(context.getResources().getString(R.string.help_privacy_answer));
 
-        List<String> permissions = new ArrayList<String>();
+        List<String> permissions = new ArrayList<>();
         permissions.add(context.getResources().getString(R.string.help_permission_answer));
 
         expandableListDetail.put(context.getResources().getString(R.string.help_privacy), privacy);
