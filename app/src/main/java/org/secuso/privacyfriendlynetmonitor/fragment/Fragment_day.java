85daf72884058f0f618aba3827ce8d32c7efc91c
tobsterlobster <tobias.burger@stud.tu-darmstadt.de>
2018-01-19 17:51:03 +0100

    added some strings and added some decleractions in every class

--- app/src/main/java/org/secuso/privacyfriendlynetmonitor/fragment/Fragment_day.java
+++ app/src/main/java/org/secuso/privacyfriendlynetmonitor/fragment/Fragment_day.java
@@ -1,5 +1,55 @@
+/*
+    Privacy Friendly Net Monitor (Net Monitor)
+    - Copyright (2015 - 2017) Felix Tsala Schiller
+
+    ###################################################################
+
+    This file is part of Net Monitor.
+
+    Net Monitor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Net Monitor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Net Monitor.  If not, see <http://www.gnu.org/licenses/>.
+
+    Diese Datei ist Teil von Net Monitor.
+
+    Net Monitor ist Freie Software: Sie können es unter den Bedingungen
+    der GNU General Public License, wie von der Free Software Foundation,
+    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+    Net Monitor wird in der Hoffnung, dass es nützlich sein wird, aber
+    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+    Siehe die GNU General Public License für weitere Details.
+
+    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+
+    ###################################################################
+
+    This app has been created in affiliation with SecUSo-Department of Technische Universität
+    Darmstadt.
+
+    The design is based on the Privacy Friendly Example App template by Karola Marky, Christopher
+    Beckmann and Markus Hau (https://github.com/SecUSo/privacy-friendly-app-example).
+
+    Privacy Friendly Net Monitor is based on TLSMetric by Felix Tsala Schiller
+    https://bitbucket.org/schillef/tlsmetric/overview.
+
+ */
+
 package org.secuso.privacyfriendlynetmonitor.fragment;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -133,6 +183,7 @@ public class Fragment_day extends Fragment {
     }
 
     private void fillChart(View view, BarChart chart) {
+
         //Handling the current time in Hour
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         Date currentTime=null;
@@ -180,8 +231,8 @@ public class Fragment_day extends Fragment {
                     cache24hours_encrypted[i],cache24hours_unencrypted[i]}));
         }
 
-        BarDataSet barset = new BarDataSet(entry, "Hours"); //TODO put in in german as well
-        barset.setStackLabels(new String[]{"Unknown", "Encrypted", "Unencrypted"}); //TODO put in in german as well
+        BarDataSet barset = new BarDataSet(entry, Fragment_day.this.getResources().getString(R.string.hours));
+        barset.setStackLabels(new String[]{Fragment_day.this.getResources().getString(R.string.unknown), Fragment_day.this.getResources().getString(R.string.encrypted), Fragment_day.this.getResources().getString(R.string.unencrypted)});
         barset.setColors(new int[] {ContextCompat.getColor(getContext(), R.color.text_dark),
                 ContextCompat.getColor(getContext(), R.color.green),
                 ContextCompat.getColor(getContext(), R.color.red)});
@@ -193,9 +244,9 @@ public class Fragment_day extends Fragment {
 
         for(int i = 0; i<hours.length; i++){
             if(i == hours.length-1){
-                hours[i] = currentHour+ " o'clock"; //TODO add to german lang
+                hours[i] = currentHour+ Fragment_day.this.getResources().getString(R.string.oclock);
             }else {
-                hours[i] = "- " + Integer.toString(23 - i) + "hr"; //TODO add to german lang
+                hours[i] = "- " + Integer.toString(23 - i) + Fragment_day.this.getResources().getString(R.string.hr);
             }
         }
 
--- app/src/main/java/org/secuso/privacyfriendlynetmonitor/fragment/Fragment_month.java
+++ app/src/main/java/org/secuso/privacyfriendlynetmonitor/fragment/Fragment_month.java
@@ -1,3 +1,52 @@
+/*
+    Privacy Friendly Net Monitor (Net Monitor)
+    - Copyright (2015 - 2017) Felix Tsala Schiller
+
+    ###################################################################
+
+    This file is part of Net Monitor.
+
+    Net Monitor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Net Monitor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Net Monitor.  If not, see <http://www.gnu.org/licenses/>.
+
+    Diese Datei ist Teil von Net Monitor.
+
+    Net Monitor ist Freie Software: Sie können es unter den Bedingungen
+    der GNU General Public License, wie von der Free Software Foundation,
+    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+    Net Monitor wird in der Hoffnung, dass es nützlich sein wird, aber
+    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+    Siehe die GNU General Public License für weitere Details.
+
+    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+
+    ###################################################################
+
+    This app has been created in affiliation with SecUSo-Department of Technische Universität
+    Darmstadt.
+
+    The design is based on the Privacy Friendly Example App template by Karola Marky, Christopher
+    Beckmann and Markus Hau (https://github.com/SecUSo/privacy-friendly-app-example).
+
+    Privacy Friendly Net Monitor is based on TLSMetric by Felix Tsala Schiller
+    https://bitbucket.org/schillef/tlsmetric/overview.
+
+ */
+
 package org.secuso.privacyfriendlynetmonitor.fragment;
 
 import android.content.pm.PackageManager;
@@ -164,8 +213,8 @@ public class Fragment_month extends Fragment {
                     lastMonth_encrypted[i],lastMonth_unencrypted[i]}));
         }
 
-        BarDataSet barset = new BarDataSet(entry, "Days"); //TODO put in in german as well
-        barset.setStackLabels(new String[]{"Unknown", "Encrypted", "Unencrypted"}); //TODO put in in german as well
+        BarDataSet barset = new BarDataSet(entry, Fragment_month.this.getResources().getString(R.string.days));
+        barset.setStackLabels(new String[]{Fragment_month.this.getResources().getString(R.string.unknown), Fragment_month.this.getResources().getString(R.string.encrypted), Fragment_month.this.getResources().getString(R.string.unencrypted)});
         barset.setColors(new int[] {ContextCompat.getColor(getContext(), R.color.text_dark),
                 ContextCompat.getColor(getContext(), R.color.green),
                 ContextCompat.getColor(getContext(), R.color.red)});
@@ -177,9 +226,9 @@ public class Fragment_month extends Fragment {
 
         for(int i = 0; i<days.length; i++){
             if(i == days.length-1){
-                days[i] = currentDay+ " ."; //TODO add to german lang
+                days[i] = currentDay+ " .";
             }else {
-                days[i] = "- " + Integer.toString(29 - i) + "d"; //TODO add to german lang
+                days[i] = "- " + Integer.toString(29 - i) + Fragment_month.this.getResources().getString(R.string.d);
             }
         }
 
